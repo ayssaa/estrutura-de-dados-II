1d04673dbc9c13ec641f41eb1f49d76256c899ba
ayssa <rarayssah@gmail.com>
2025-11-04 11:37:59 -0300

    alteracao simples
    Added code to display the unsorted array before sorting.

--- atividades/bolha.c
+++ atividades/bolha.c
@@ -8,6 +8,14 @@ void main() {
     int n = 3;
     int aux;
 
+    // Exibe vetor desordenado
+    printf("Vetor ordenado: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", vet[i]);
+    }
+
+    printf("\n");
+
     for (int i = n-1; i >= 1; i--) {
         for (int j = 0; j < i; j++) {
             if (vet[j] > vet[j+1]) {
